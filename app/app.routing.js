0aecccc8620bdca69c971dff93b00eddf77647ea
Teulys Jiménez <teulys@gmail.com>
2018-08-04 20:22:05 -0400

    First Commit

---
+++ app/app.routing.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var home_component_1 = require("~/home/home.component");
+exports.routes = [
+    { path: "", component: home_component_1.HomeComponent }
+];
+exports.navigatableComponents = [
+    home_component_1.HomeComponent
+];
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLnJvdXRpbmcuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJhcHAucm91dGluZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLHdEQUFzRDtBQUd6QyxRQUFBLE1BQU0sR0FBRztJQUNsQixFQUFFLElBQUksRUFBRSxFQUFFLEVBQUUsU0FBUyxFQUFFLDhCQUFhLEVBQUU7Q0FDekMsQ0FBQztBQUVXLFFBQUEscUJBQXFCLEdBQUc7SUFDakMsOEJBQWE7Q0FDaEIsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IEhvbWVDb21wb25lbnQgfSBmcm9tIFwifi9ob21lL2hvbWUuY29tcG9uZW50XCI7XG5cblxuZXhwb3J0IGNvbnN0IHJvdXRlcyA9IFtcbiAgICB7IHBhdGg6IFwiXCIsIGNvbXBvbmVudDogSG9tZUNvbXBvbmVudCB9XG5dO1xuXG5leHBvcnQgY29uc3QgbmF2aWdhdGFibGVDb21wb25lbnRzID0gW1xuICAgIEhvbWVDb21wb25lbnRcbl07Il19
---
+++ app/app.routing.ts
@@ -0,0 +1,10 @@
+import { HomeComponent } from "~/home/home.component";
+
+
+export const routes = [
+    { path: "", component: HomeComponent }
+];
+
+export const navigatableComponents = [
+    HomeComponent
+];
